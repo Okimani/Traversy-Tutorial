cb427a85712ad1d5dc9879dbb0446bd08f25c985
Oscar Kimani <okimani7@gmail.com>
2021-12-01 12:42:09 +0300

    added html and Javascript file

---
+++ main.js
@@ -0,0 +1,19 @@
+// strings, numbers, boolean, null, undefined , symbol
+//const names ='John';
+//const age = 30;
+//const iscool =true;
+//const rating =4.5;
+//const x =null;
+//const y = undefined;
+//let z
+
+//console.log(typeof names);
+
+const names ='John';
+const age = 30;
+//concatenation
+//old method 
+//console.log ('my name is ' + names + ' and I am '+ age);
+
+console.log(`My name is  ${names} and I am ${age} years old`);
+
